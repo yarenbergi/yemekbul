bba41b39a94b667dbc076b9cd3b82d2e1e25a04f
yaren <yarenbergi25@gmail.com>
2021-04-22 19:15:30 +0300

    deneme iki

--- app/src/main/java/com/yarenbergi/yemekbul/MainActivity.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     //oytun
+    //yaren
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
