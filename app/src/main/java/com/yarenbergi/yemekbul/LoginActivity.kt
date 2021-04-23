580c93817de052216168e9a60dea2273a1bf5334
yaren <yarenbergi25@gmail.com>
2021-04-23 21:11:42 +0300

    login ekranı güncelleme v2

--- app/src/main/java/com/yarenbergi/yemekbul/LoginActivity.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/LoginActivity.kt
@@ -2,10 +2,68 @@ package com.yarenbergi.yemekbul
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        buttonLogin.setOnClickListener {
+            validate()
+        }
+
     }
+
+    private fun validate(){
+        val isEmailValid = validateEmail()
+        val isPasswordValid = validatePassword()
+
+        if (isEmailValid && isPasswordValid){
+            showSucces()
+        }
+    }
+
+    private fun validateEmail():Boolean{
+        val email= editTextEmail.text.toString()
+
+        if(email.isEmpty()){
+            textInputLayoutEmail.error = getString(R.string.emailEmptyErrorMessage)
+            return false
+        }
+
+        if(Patterns.EMAIL_ADDRESS.matcher(email).matches().not()){
+            textInputLayoutEmail.error = getString(R.string.emailInvalidErrorMessage)
+            return false
+        }
+
+        textInputLayoutEmail.error = null
+        return true
+    }
+
+    private fun validatePassword():Boolean{
+        val password = editTextPassword.text.toString()
+
+        if(password.length < 8){
+            textInputLayoutPassword.error = getString(R.string.passwordInvalidErrorMessage)
+            return false
+        }
+
+        textInputLayoutPassword.error = null
+        return true
+    }
+
+    fun showSucces(){
+        val email = editTextEmail.text.toString()
+        val name = email.substringBefore("@")
+        toast(getString(R.string.loginSucces, name))
+    }
+
+    private fun toast(text: String){
+        Toast.makeText(this, "Hoşgeldiniz!", Toast.LENGTH_LONG).show()
+    }
+
 }
--- app/src/main/java/com/yarenbergi/yemekbul/MainActivity.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yarenbergi.yemekbul
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     //oytun
@@ -10,6 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        uyeOlbutton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
