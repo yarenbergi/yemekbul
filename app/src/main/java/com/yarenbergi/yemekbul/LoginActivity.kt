133fb92e06dee178bba8d2d59368c9cfd8343af9
yaren <yarenbergi25@gmail.com>
2021-04-23 13:36:47 +0300

    login ekranı güncelleme

--- app/src/main/java/com/yarenbergi/yemekbul/LoginActivity.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/LoginActivity.kt
@@ -1,15 +1,11 @@
 package com.yarenbergi.yemekbul
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    //oytun
-    //yaren
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_login)
     }
 }
--- app/src/main/java/com/yarenbergi/yemekbul/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.yarenbergi.yemekbul.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
