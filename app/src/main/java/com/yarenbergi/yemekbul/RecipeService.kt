9e9f37f81c64847b9f5aea27d85592ece5a5399f
sevgiaydn <sevgiaydin1999@hotmail.com>
2021-06-01 10:36:36 +0300

    recipe class ve service eklendi

--- app/src/main/java/com/yarenbergi/yemekbul/RecipeService.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/RecipeService.kt
@@ -3,13 +3,17 @@ package com.yarenbergi.yemekbul
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.yarenbergi.yemekbul.data.MenuItems
+import com.yarenbergi.yemekbul.data.Recipe
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RecipeService {
-    //@Headers("apiKey: " + "e1de17cefea24c36a776f31b15536d71")
-    @GET("/food/menuItems/424571?apiKey=e1de17cefea24c36a776f31b15536d71&includeNutrition=true")
-    fun getMenuItem(): Call<MenuItems>
+    @GET("/food/menuItems/{id}")
+    fun getMenuItem(@Path("id") id: String, @Query("apiKey") apiKey:String)
+
+    @GET("/recipes/{id}/similar")
+    fun getSimilarRecipes(@Path("id") id: String, @Query("apiKey") apiKey:String): Call<List<Recipe>>
 }
---
+++ app/src/main/java/com/yarenbergi/yemekbul/data/Recipe.kt
@@ -0,0 +1,16 @@
+package com.yarenbergi.yemekbul.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Recipe(@SerializedName("readyInMinutes")
+                  val readyInMinutes: Int = 0,
+                  @SerializedName("sourceUrl")
+                  val sourceUrl: String = "",
+                  @SerializedName("servings")
+                  val servings: Int = 0,
+                  @SerializedName("id")
+                  val id: Int = 0,
+                  @SerializedName("title")
+                  val title: String = "",
+                  @SerializedName("imageType")
+                  val imageType: String = "")
