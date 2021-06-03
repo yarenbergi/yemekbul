5cf7a0c6291a72bf84a1c78fdc4d3936acdaf3de
yaren <yarenbergi25@gmail.com>
2021-06-03 17:28:39 +0300

    ui kısmı

--- app/src/main/java/com/yarenbergi/yemekbul/MainActivity.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/MainActivity.kt
@@ -4,34 +4,43 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
+            setOf(
+            R.id.recipesFragment,
+            R.id.favoriteRecipesFragment,
+            R.id.shoppingListFragment
+         )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
 
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
 
         StrictMode.setThreadPolicy(policy)
 
-        uyeOlbutton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
 
-        girisYapbutton.setOnClickListener {
-            val intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
-        }
+    }
 
-        getMenuItem.setOnClickListener {
-            val intent = Intent(this, MenuItemActivity::class.java)
-            startActivity(intent)
-        }
-
-
-
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
