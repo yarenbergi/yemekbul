a81155d5cc822197d87694a2ed88189e0ea203ee
yaren <yarenbergi25@gmail.com>
2021-06-11 20:25:00 +0300

    filtre kısmı meal type eklendi

---
+++ app/src/main/java/com/yarenbergi/yemekbul/data/bottomsheet/RecipesBottomSheet.kt
@@ -0,0 +1,19 @@
+package com.yarenbergi.yemekbul.data.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.yarenbergi.yemekbul.R
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+
+}
