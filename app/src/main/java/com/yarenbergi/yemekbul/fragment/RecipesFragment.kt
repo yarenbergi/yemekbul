c4e8be6fc1588d3bc48b07bc9e7202bbaef504d2
yaren <yarenbergi25@gmail.com>
2021-06-13 14:58:04 +0300

    filtre kısmı tamamlandı ve search kısmı eklendi.

--- app/src/main/java/com/yarenbergi/yemekbul/fragment/RecipesFragment.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/fragment/RecipesFragment.kt
@@ -2,10 +2,9 @@ package com.yarenbergi.yemekbul.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -19,11 +18,13 @@ import com.yarenbergi.yemekbul.Service
 import com.yarenbergi.yemekbul.activity.MenuItemActivity
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
-class RecipesFragment : Fragment() {
+class RecipesFragment : Fragment(), SearchView.OnQueryTextListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_recipes, container, false)
@@ -33,10 +34,31 @@ class RecipesFragment : Fragment() {
         recyclerView.adapter= recipes?.let { RecyclerviewAdapter_Recipes(it) }
     //    view.recycler_view.showShimmer() //TODO: bu olunca yazdırmıyor!
 
+        setHasOptionsMenu(true)
+
+
+
         view.recipes_fab.setOnClickListener {
             findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
         }
 
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.recipes_menu, menu)
+
+        val search = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        return true
     }
 }
