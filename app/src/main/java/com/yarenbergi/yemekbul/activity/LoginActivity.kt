3dd362be130d27703247c4e034dfc3c02f0b3a33
sevgiaydn <sevgiaydin1999@hotmail.com>
2021-06-28 20:45:47 +0300

    cloud user eklendi

--- app/src/main/java/com/yarenbergi/yemekbul/activity/LoginActivity.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/activity/LoginActivity.kt
@@ -1,70 +1,150 @@
 package com.yarenbergi.yemekbul.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.yarenbergi.yemekbul.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
+
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // [END initialize_auth]
 
-        buttonLogin.setOnClickListener {
-            validate()
+        val signInButton : Button = findViewById(R.id.buttonSignIn)
+        val signUpButton : Button = findViewById(R.id.buttonSignUp)
+        val loginWithoutButton : Button = findViewById(R.id.buttonWithoutLogin)
+        signInButton.setOnClickListener {
+            val mailText : EditText = findViewById(R.id.textInputLayoutEmail)
+            val password : EditText = findViewById(R.id.textInputLayoutPassword)
+            signIn(mailText.text.toString().trim(), password.text.toString().trim())
         }
+        signUpButton.setOnClickListener {
+            val mailText : EditText = findViewById(R.id.textInputLayoutEmail)
+            val password : EditText = findViewById(R.id.textInputLayoutPassword)
+            createAccount(mailText.text.toString().trim(), password.text.toString().trim())
+        }
+        loginWithoutButton.setOnClickListener {
+            signInAnonymously()
+        }
+    }
+
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+    // [END on_start_check_user]
+
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                    intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                    intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun signInAnonymously() {
+        // [START signin_anonymously]
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInAnonymously:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                    intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInAnonymously:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END signin_anonymously]
+    }
+
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // Email Verification sent
+            }
+        // [END send_email_verification]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
 
     }
 
-    private fun validate(){
-        val isEmailValid = validateEmail()
-        val isPasswordValid = validatePassword()
+    private fun reload() {
 
-        if (isEmailValid && isPasswordValid){
-            showSucces()
-        }
     }
 
-    private fun validateEmail():Boolean{
-        val email= editTextEmail.text.toString()
-
-        if(email.isEmpty()){
-            textInputLayoutEmail.error = getString(R.string.emailEmptyErrorMessage)
-            return false
-        }
-
-        if(Patterns.EMAIL_ADDRESS.matcher(email).matches().not()){
-            textInputLayoutEmail.error = getString(R.string.emailInvalidErrorMessage)
-            return false
-        }
-
-        textInputLayoutEmail.error = null
-        return true
-    }
-
-    private fun validatePassword():Boolean{
-        val password = editTextPassword.text.toString()
-
-        if(password.length < 8){
-            textInputLayoutPassword.error = getString(R.string.passwordInvalidErrorMessage)
-            return false
-        }
-
-        textInputLayoutPassword.error = null
-        return true
-    }
-
-    fun showSucces(){
-        val email = editTextEmail.text.toString()
-        val name = email.substringBefore("@")
-        toast(getString(R.string.loginSucces, name))
-    }
-
-    private fun toast(text: String){
-        Toast.makeText(this, "Hoşgeldiniz!", Toast.LENGTH_LONG).show()
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 
 }
--- app/src/main/java/com/yarenbergi/yemekbul/activity/MainActivity.kt
+++ app/src/main/java/com/yarenbergi/yemekbul/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.yarenbergi.yemekbul.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.yarenbergi.yemekbul.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,11 +19,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if(currentUser == null){
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
         navController = findNavController(R.id.navHostFragment)
         val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
             setOf(
